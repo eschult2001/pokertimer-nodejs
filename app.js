b9bd54d016a7eba631248f55e72aa574203a0db5
Eric Schult <eschult2001@yahoo.com>
2015-02-08 08:09:41 -0500

    OpenShift compatibility

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 // define globals
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
@@ -12,8 +14,8 @@ var io = require('socket.io')(http);
 require('./sockets/base')(io);
 
 // start the server
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + server_port )
 });
 
 // Static Content
