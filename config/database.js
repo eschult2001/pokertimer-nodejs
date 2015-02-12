a4eac760cfc76397f7aa59b348a1970a9c22409c
Eric Schult <eschult2001@yahoo.com>
2015-02-12 18:03:49 -0500

    Fixed MongoDB url from env vars

--- config/database.js
+++ config/database.js
@@ -1,8 +1,5 @@
 // config/database.js
-// mongodb://$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/
-var dbhost = process.env.OPENSHIFT_MONGODB_DB_HOST || 'localhost';
-var dbport = process.env.OPENSHIFT_MONGODB_DB_PORT || 27017;
 
 module.exports = {
-	'url' : 'mongodb://' + dbhost + ':' + dbport + '/timer'
+	'url' : process.env.OPENSHIFT_MONGODB_DB_URL || 'mongodb://localhost/timer' 
 };
